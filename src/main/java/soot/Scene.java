21cf7160c4e5d3527ee57acf2e43a162489204a5
Gauravsingh Sisodia <xaerru@gmail.com>
2026-05-02 05:51:20 +0000

    fix: synchronize SmallNumberedMap and add Field.toGenericString to reflection trace classes

--- src/main/java/soot/Scene.java
+++ src/main/java/soot/Scene.java
@@ -1903,6 +1903,7 @@ public class Scene {
               case "Field.set*":
               case "Field.get*":
               case "Field.toString":
+              case "Field.toGenericString":
               case "Field.getName":
               case "Field.getDeclaringClass":
                 classNames.add(signatureToClass(target));
